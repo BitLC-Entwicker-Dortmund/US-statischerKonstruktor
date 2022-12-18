989154959c75571d9693395ef22d95c6db37e9ac
k s <klaus.stolz@web.de>
2022-12-18 12:15:30 +0100

    Add project files.

---
+++ MeineStatischeKlasse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace US_statischerKonstruktor {
+    static class MeineStatischeKlasse {
+
+        static MeineStatischeKlasse() {
+            Console.WriteLine ( "Hallo vom statischen Konstruktor");
+        }
+
+        public static int Rechne ( int z1, int z2) {
+            return z1 + z2;
+        }
+
+        public static void MachMal ( ) {
+            Console.WriteLine ( "danach MachMal..." );
+            Console.WriteLine ("Zuerst kommt der Konstruktor, dann alles Andere ...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace US_statischerKonstruktor {
+    class Program {
+        static void Main ( string [ ] args ) {
+
+            MeineStatischeKlasse.MachMal();
+
+            Console.ReadLine ( );
+        }
+    }
+}
